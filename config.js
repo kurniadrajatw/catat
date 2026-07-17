cb7fe123c93f67ca7a2720923b389d3ab17917e0
kurniadrajatw <49942975+kurniadrajatw@users.noreply.github.com>
2026-07-17 09:32:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // SUPABASE_ANON_KEY aman ditaruh di frontend selama Row Level Security (RLS)
 // sudah diaktifkan sesuai instruksi di README.md / schema.sql
 
-const SUPABASE_URL = "ISI-DENGAN-URL-PROJECT-SUPABASE";
-const SUPABASE_ANON_KEY = "ISI-DENGAN-ANON-KEY-SUPABASE";
+const SUPABASE_URL = "https://ukjcwftqsjqqeioiifes.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVramN3ZnRxc2pxcWVpb2lpZmVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzE2ODIsImV4cCI6MjA5OTgwNzY4Mn0.01Pr6N51aMFyKWp9cU3bOTaudfe4DSC4S5U8yFABBeE";
